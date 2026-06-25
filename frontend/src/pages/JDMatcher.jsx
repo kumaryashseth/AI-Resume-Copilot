58284ae6ce2daf45f0b3cdc8fc3010bd39c4a38f
kumaryashseth <yashv3434@gmail.com>
2026-06-26 02:05:23 +0530

    added fronted for job description and resume rewriter

--- frontend/src/pages/JDMatcher.jsx
+++ frontend/src/pages/JDMatcher.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { analyzeJD } from "../services/analysisService";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const JDMatcher = () => {
-  return (
-    <div>JDMatcher</div>
-  )
-}
+  const [resumeText, setResumeText] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
+  const [result, setResult] = useState(null);
 
-export default JDMatcher
+  const handleAnalysis = async () => {
+    const res = await analyzeJD({
+      resumeText,
+      jobDescription,
+    });
+    setResult(res.data);
+  };
+
+  return (
+    <Box>
+      <TextField
+        multiline
+        rows={6}
+        fullWidth
+        label="Resume Text"
+        onChange={(e) => setResumeText(e.target.value)}
+      />
+      <TextField
+        multiline
+        rows={6}
+        fullWidth
+        label="Job Description"
+        onChange={(e) => setJobDescription(e.target.value)}
+      />
+      <Button variant="contained" onClick={handleAnalysis}>Analyze</Button>
+    {
+      result && (
+        <div>
+          <Typography variant="h6">Result</Typography>
+          <Typography>{result.matchScore}</Typography>
+        </div>
+      )
+    }
+    
+    </Box>
+  );
+};
+
+export default JDMatcher;
--- frontend/src/pages/ResumeRewriter.jsx
+++ frontend/src/pages/ResumeRewriter.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { rewritePoint } from '../services/analysisService';
+import {TextField, Button} from '@mui/material'
 
 const ResumeRewriter = () => {
+  
+  const [point, setPoint] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleRewrite = async () => {
+    
+    const res = await rewritePoint({bulletPoint: point});
+    setResult(res.data.improved);
+  }
   return (
-    <div>ResumeRewriter</div>
+    <div>
+      <TextField
+        label="Enter bullet point"
+        value={point}
+        onChange={(e) => setPoint(e.target.value)}
+        multiline
+        rows={4}
+        fullWidth
+      />
+      <Button variant='contained' onClick={handleRewrite}>Rewrite</Button>
+      
+      <div className='mt-6 p-4 border'>
+        {result}
+      </div>
+      </div>
   )
 }
 
