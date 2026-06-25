f9f7e44698b8c21704352741f1aef26a4f4a5554
kumaryashseth <yashv3434@gmail.com>
2026-06-25 22:28:57 +0530

    Added frontend UploadResume

--- frontend/src/pages/UploadResume.jsx
+++ frontend/src/pages/UploadResume.jsx
@@ -1,10 +1,43 @@
-
-import React from 'react'
+import React, { useState } from "react";
+import { Button, LinearProgress, Paper, Typography } from "@mui/material";
+import { uploadResume } from "../services/analysisService";
 
 const UploadResume = () => {
-  return (
-    <div>UploadResume</div>
-  )
-}
+  const [file, setFile] = useState(null);
 
-export default UploadResume
+  const [progress, setProgress] = useState(0);
+
+  const handleUpload = async () => {
+    if (!file) {
+      alert("Please select a PDF file first.");
+      return;
+    }
+    try {
+      const res = await uploadResume(file, (event) => {
+        const percent = Math.round((event.loaded * 100) / event.total);
+        setProgress(percent);
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Paper>
+      <Typography variant="h5">UploadResume</Typography>
+      <input
+        type="file"
+        accept=".pdf"
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+      <Button variant="contained" className="mt-4" onClick={handleUpload}>
+        Upload
+      </Button>
+      {progress > 0 && (
+        <LinearProgress value={progress} variant="determinate" />
+      )}
+    </Paper>
+  );
+};
+
+export default UploadResume;
