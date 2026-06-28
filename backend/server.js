221129d53813776a4307fecfa9b2a3e2dae83c1a
kumaryashseth <yashv3434@gmail.com>
2026-06-28 13:50:45 +0530

    ready for deployement

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extendedd: false }));
 
